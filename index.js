28f348283181eaf9e603884d70ad6f70058b0c9d
Ali GÖREN <goren.ali@yandex.com>
2017-12-30 13:30:06 +0300

    Added count's sample usage

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import Util from "./util";
+
 const u = new Util();
 
 console.log(u.format('Hi {0}. Did you see the {1}?', ['Pikachu', 'Meow']));
@@ -13,8 +15,16 @@ console.log(u.is(5.1, u.Integer));
 console.log(u.is(3.2, u.Double));
 
 console.log(u.is('Ali' + 123, u.String));
-
-console.log(u.is(a = (d) => {}, u.Function));
+const a = (d) => {}
+console.log(u.is(a, u.Function));
 console.log(u.is(function a (d) {}, u.Function));
 
 console.log(u.is(class b {}, u.Class));
+
+console.log(u.count('My name is no name when I do not like names. What is your name? Can u say your naming conversion', 'name'));
+
+console.log(u.count('😚 You now I\'ll be happy', u.Letter));
+
+console.log(u.count([1, 2, 3], u.Array));
+
+console.log(u.count({'w': 't', 'w2': 't2', 'wf': 'wf', 'wfs': 'wfs2'}, u.Object));
